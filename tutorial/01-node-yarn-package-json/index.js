0d2a25d2222cc44e4309b197f2da6a13b971be67
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-15 21:47:51 +0700

    Lowercase color, checkered flag emoji attemps

--- tutorial/01-node-yarn-package-json/index.js
+++ tutorial/01-node-yarn-package-json/index.js
@@ -1,5 +1,5 @@
-const Color = require('color')
+const color = require('color')
 
-const redHexa = Color({ r: 255, g: 0, b: 0 }).hex()
+const redHexa = color({ r: 255, g: 0, b: 0 }).hex()
 
 console.log(redHexa)
