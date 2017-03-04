15d81676873d1fc43db1be6c95599767408941e3
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-03-04 23:30:13 +0800

    2.4.2 walkthrough done for chapter 04

--- tutorial/04-webpack-react-hmr/code/src/client/index.jsx
+++ tutorial/04-webpack-react-hmr/code/src/client/index.jsx
@@ -4,5 +4,26 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<h1>Hello React!</h1>, document.querySelector('.app'))
+import App from './app'
+import { APP_CONTAINER_SELECTOR } from '../shared/config'
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = AppComponent =>
+  <AppContainer>
+    <AppComponent />
+  </AppContainer>
+
+ReactDOM.render(wrapApp(App), rootEl)
+
+if (module.hot) {
+  // flow-disable-next-line
+  module.hot.accept('./app', () => {
+    /* eslint-disable global-require */
+    const NextApp = require('./app').default
+    /* eslint-enable global-require */
+    ReactDOM.render(wrapApp(NextApp), rootEl)
+  })
+}
--- tutorial/04-webpack-react-hmr/code/src/server/require-hook.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-require('babel-register')
-require('./index.js')
